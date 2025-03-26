22c7040d95f7396cd6be73dca50e84df661e86b4
maseratirex <r.jamesthomaswalker@gmail.com>
2025-03-25 20:47:32 -0400

    Implement basic Likert Scale

--- app/(assessment)/section/[name].tsx
+++ app/(assessment)/section/[name].tsx
@@ -1,8 +1,18 @@
 import { Text, View } from "react-native";
 import { useLocalSearchParams } from 'expo-router';
+import LikertScale from '@/components/LikertScale'
 
 export default function Assessment2Screen() {
     const { name } = useLocalSearchParams();
+    // May need to convert "name" to lowercase
+    // Example questions and answers. Should actually be based on "name"
+    const questions = [
+        "I feel comfortable using this app.",
+        "The app's layout is intuitive.",
+        "I would recommend this app to others.",
+        "This app meets my needs."
+    ];
+    const answers = "0".repeat(questions.length);
 
     return (
         <View
@@ -13,6 +23,8 @@ export default function Assessment2Screen() {
             }}
         >
             <Text>Section {name}</Text>
+            <Text>{answers}</Text>
+            <LikertScale questions={questions} answers={answers} />
         </View>
     );
 }
