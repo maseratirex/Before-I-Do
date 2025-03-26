e4f17675da4f2177524a5f909ba364e8289d857e
Soumya Mahavadi <soumyasri.340@gmail.com>
2025-03-25 22:43:00 -0400

    Updated to import questionnaire data.

--- app/(assessment)/section/[name].tsx
+++ app/(assessment)/section/[name].tsx
@@ -1,17 +1,14 @@
 import { Text, View } from "react-native";
 import { useLocalSearchParams } from 'expo-router';
 import LikertScale from '@/components/LikertScale'
+import { questionnaire } from "./questionnaire";
+
+type SectionName = 'personality' | 'family' | 'couple' | 'cultural';
 
 export default function Assessment2Screen() {
-    const { name } = useLocalSearchParams();
-    // May need to convert "name" to lowercase
-    // Example questions and answers. Should actually be based on "name"
-    const questions = [
-        "I feel comfortable using this app.",
-        "The app's layout is intuitive.",
-        "I would recommend this app to others.",
-        "This app meets my needs."
-    ];
+    const { name } = useLocalSearchParams<{ name: string }>();
+    const section = name.toLowerCase() as SectionName;
+    const questions = questionnaire[section];
     const answers = "0".repeat(questions.length);
 
     return (
