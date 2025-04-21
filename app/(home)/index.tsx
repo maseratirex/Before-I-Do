5ae3126beaae2e5983cdc9a1b146433a8e7d6567
Osoo Kwon <osoojkwon@gmail.com>
2025-04-21 15:58:16 -0400

    Work on navbar

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -1,7 +1,8 @@
-import { View, StyleSheet, Pressable, Text } from "react-native";
-import { useRouter } from "expo-router";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Link, useRouter } from "expo-router";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from '@/firebaseConfig'
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Index() {
   const router = useRouter();
@@ -13,16 +14,17 @@ export default function Index() {
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
-      <Pressable style={styles.button} onPress={() => router.push("/directory")}>
-        <Text style={styles.buttonText}>Begin assessment</Text>
-      </Pressable>
-    </View>
+    <LinearGradient colors={['#FFE4EB', '#FFC6D5']} style={styles.container}>
+        <TouchableOpacity style={styles.box} onPress={() => router.push("/directory")}>
+          <Text style={styles.title}>Assessment</Text>
+          <Text style={styles.description}>
+            Discover insights about yourself and your relationship
+          </Text>
+          <View style={styles.divider} />
+          <Text style={styles.actionText}>Begin Assessment</Text>
+        </TouchableOpacity>
+      <Text style={styles.title}>Pair with Partner</Text>
+    </LinearGradient>
   );
 }
 
@@ -33,6 +35,37 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         padding: 20,
         backgroundColor: '#fff',
+    },
+    box: {
+      width: "90%",
+      paddingVertical: 20,
+      paddingHorizontal: 15,
+      backgroundColor: "white",
+      borderRadius: 16,
+      justifyContent: "center",
+      marginBottom: 20,
+      shadowColor: "#000",
+      shadowOffset: { width: 0, height: 4 },
+      shadowOpacity: 0.2,
+      shadowRadius: 2,
+      elevation: 6,
+    },
+    description: {
+      fontSize: 14,
+      color: "#333",
+      marginBottom: 10,
+    },
+    divider: {
+      width: "100%",
+      height: 1,
+      backgroundColor: "#ccc",
+      marginVertical: 10,
+    },
+    actionText: {
+      fontSize: 15,
+      fontWeight: "bold",
+      color: "#4a4a4a",
+    
     },
     title: {
         fontSize: 24,
