598e5221ec1d221296d6f7ba46f612bd1e5df655
Osoo Kwon <osoojkwon@gmail.com>
2025-05-01 13:52:21 -0400

    Pair partner UI

--- components/PairPartnerInfo.jsx
+++ components/PairPartnerInfo.jsx
@@ -1,12 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
-import { functions } from "@/firebaseConfig";
+import { functions, db } from "@/firebaseConfig";
 import { httpsCallable } from "firebase/functions";
 import { getAuth } from "firebase/auth";
 import { MaterialIcons } from "@expo/vector-icons";
+import { doc, getDoc } from "firebase/firestore";
+
 
 
 const PairingInfo = ({ isPaired, setIsPaired, hasSentRequest, numRecievedRequest, setRequests, partnerInitials, setPartnerInitials, partnerEmail, setPartnerEmail }) => {
+    const [userInitial, setUserInitial] = useState('');
     const unpairUsers = async () => {
         try {
             const unpairFunction = httpsCallable(functions, "unpair");
@@ -61,34 +64,66 @@ const PairingInfo = ({ isPaired, setIsPaired, hasSentRequest, numRecievedRequest
         }
     }
 
+    const getUserInitials = async () => {
+        const auth = getAuth();
+        const user = auth.currentUser;
+        const userRef = doc(db, "users", user.uid);
+        const docSnap = await getDoc(userRef);
+        if (docSnap.exists()) {
+            const data = docSnap.data();
+            setUserInitial(data.initials);
+        }
+    }
+
+    useEffect(() => {
+        getUserInitials();
+    }, []);
+
+
     const isPairedReturn = () => {
         return (
-            <View>
-                <Text style={styles.title}>Pair Partner</Text>
-                <Text>{"You are paired with " + partnerInitials + " (" + partnerEmail + ")."}</Text>
+            <View style={{ alignItems: 'center', marginTop: 5 }}>
+                <Text style={styles.title}>Paired</Text>
+                <View style={styles.overlapContainer}>
+                    <View style={[styles.circle, styles.leftCircle]}>
+                        <Text style={styles.initialsText}>{userInitial}</Text>
+                    </View>
+                    <View style={[styles.circle, styles.rightCircle,]}>
+                        <Text style={styles.initialsText}>{partnerInitials}</Text>
+                    </View>
+                </View>
+                {/* <Text style={{ marginTop: 10 }}>
+                    You are paired with {partnerInitials} ({partnerEmail}).
+                </Text> */}
                 <TouchableOpacity style={styles.button} onPress={unpairUsers}>
-                    <Text>Unpair</Text>
+                    <Text style={styles.buttonText}>Unpair{'\n'}{partnerEmail}</Text>
                 </TouchableOpacity>
             </View>
         );
-    }
+    };
 
     const isNotPairedReturn = () => {
         return (
             <>
                 <View style={styles.notPairedContainer}>
                     <Text style={styles.title}>Pair Partner</Text>
-                    <TouchableOpacity style={styles.refreshButton} onPress={seePairRequests}>
-                        <MaterialIcons name="refresh" size={36} />
-                    </TouchableOpacity>
+                    <Text style={styles.pairDescription}>Discover your relationship’s strengths together</Text>
+                    <View style={styles.divider} />
+                    <View style={styles.refreshBox}>
+                        <Text style={styles.refreshText}>Check for invites</Text>
+                        <TouchableOpacity style={styles.refreshButton} onPress={seePairRequests}>
+                            <MaterialIcons name="refresh" size={36} />
+                        </TouchableOpacity>
+                    </View>
+                    <View style={styles.divider} />
                 </View>
                 {hasSentRequest ? (
                     <>
-                        <Text>You have sent a pairing request.</Text>
+                        {/* <Text>You have sent a pairing request.</Text> */}
                     </>
                 ) : numRecievedRequest > 0 ? (
                     <>
-                        <Text>You have received {numRecievedRequest} pairing requests.</Text>
+                        {/* <Text>You have received {numRecievedRequest} pairing requests.</Text> */}
                     </>
                 ) : (
                     <>
@@ -109,7 +144,6 @@ const PairingInfo = ({ isPaired, setIsPaired, hasSentRequest, numRecievedRequest
 const styles = StyleSheet.create({
     container: {
         width: "83%",
-        paddingVertical: 20,
         paddingHorizontal: 15,
         backgroundColor: "white",
         borderRadius: 16,
@@ -124,20 +158,103 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     button: {
-        backgroundColor: '#007bff',
+        padding: 8,
+        backgroundColor: '#EEEEEE',
         alignItems: 'center',
-        borderRadius: 5,
+        borderRadius: 8,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+        shadowRadius: 1,
+
+    },
+    buttonText: {
+        color: '#4a4a4a',
+        fontSize: 15,
+        lineHeight: 15,
+        textAlign: 'center',
+        fontWeight: 'bold',
     },
     notPairedContainer: {
-        flexDirection: "row",
+        // flexDirection: "row",
         alignItems: "center",
         justifyContent: "space-between",
     },
     title: {
         fontSize: 24,
         fontWeight: 'bold',
-        marginBottom: 20,
+        textAlign: "left",
+        alignSelf: "flex-start",
+        paddingLeft: '5%',
     },
+    overlapContainer: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        marginTop: 10,
+    },
+
+    circle: {
+        width: 80,
+        height: 80,
+        borderRadius: 40,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFDDE7',
+        position: 'relative',
+        zIndex: 1,
+        borderWidth: 5,
+        borderColor: '#FFF',
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+        marginBottom: 7,
+    },
+
+    leftCircle: {
+        marginRight: -10, // creates the overlap
+        zIndex: 2,
+    },
+
+    rightCircle: {
+        zIndex: 1,
+        backgroundColor: '#DD90A8',
+    },
+
+    initialsText: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        color: '#000',
+    },
+    divider: {
+        width: "100%",
+        height: 1,
+        backgroundColor: "#ccc",
+        marginVertical: 10,
+    },
+
+    refreshBox: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        width: '100%',
+        paddingHorizontal: 10,
+    },
+
+    refreshText: {
+        fontWeight: 'bold',
+        color: '#4a4a4a',
+    },
+
+    pairDescription: {
+        textAlign: "left",
+        alignSelf: "flex-start",
+        paddingLeft: '5%',
+        color: '#4a4a4a',
+        paddingTop: 7,
+    }
 });
 
 export default PairingInfo;
--- components/PairPartnerRecievedRequests.jsx
+++ components/PairPartnerRecievedRequests.jsx
@@ -3,8 +3,9 @@ import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
 import { functions } from "@/firebaseConfig";
 import { httpsCallable } from "firebase/functions";
 import { getAuth } from "firebase/auth";
+import { Pressable } from 'react-native';
 
-const SeeRequestsComp = ({ setIsPaired, pairRequests,  setRequests, setPartnerInitials, setPartnerEmail}) => {
+const SeeRequestsComp = ({ setIsPaired, pairRequests, setRequests, setPartnerInitials, setPartnerEmail }) => {
     const numPairRequests = pairRequests.length;
     const [acceptPartner, setAcceptPartner] = useState("");
 
@@ -16,15 +17,15 @@ const SeeRequestsComp = ({ setIsPaired, pairRequests,  setRequests, setPartnerIn
             const confirmPairRequestFunction = httpsCallable(functions, "confirmPairing");
             const auth = getAuth();
             const myParams = {
-                email: numPairRequests==1 ? pairRequests[0].email : acceptPartner,
+                email: numPairRequests == 1 ? pairRequests[0].email : acceptPartner,
                 user: auth.currentUser?.uid,
             }
             const result = await confirmPairRequestFunction(myParams);
             const data = result.data;
             if (data.success) {
                 setIsPaired(true);
-                Alert.alert("Success", "Successfully confirmed pair request with " + numPairRequests==1 ? pairRequests[0].email : acceptPartner + ".");
-                console.log("Successfully paired with " + numPairRequests==1 ? pairRequests[0].email : acceptPartner + ".");
+                Alert.alert("Success", "Successfully confirmed pair request with " + numPairRequests == 1 ? pairRequests[0].email : acceptPartner + ".");
+                console.log("Successfully paired with " + numPairRequests == 1 ? pairRequests[0].email : acceptPartner + ".");
                 const seePairStatusFunction = httpsCallable(functions, "seePairStatus");
                 const auth = getAuth();
                 const myParams = {
@@ -51,13 +52,15 @@ const SeeRequestsComp = ({ setIsPaired, pairRequests,  setRequests, setPartnerIn
 
     const oneRecievedRequestReturn = () => {
         return (
-            <View> 
-                <Text style={styles.message}>{pairRequests[0].email} sent you a pair request</Text>
-                <TouchableOpacity style={styles.button} onPress={acceptPairRequest}>
+            <View>
+                <Text style={styles.warnPartAccept}>Accepting an invite shares your data with them</Text>
+                <Text style={styles.messageOne}>{pairRequests[0].email} sent you a pair request</Text>
+                <TouchableOpacity style={styles.buttonOneRequest} onPress={acceptPairRequest}>
                     <Text style={styles.buttonText}>
                         Accept
                     </Text>
                 </TouchableOpacity>
+                <View style={styles.divider} />
             </View>
         );
     }
@@ -77,52 +80,128 @@ const SeeRequestsComp = ({ setIsPaired, pairRequests,  setRequests, setPartnerIn
 
     const multiRecievedRequestsReturn = () => {
         return (
-            <View> 
-                <Text style={styles.message}>You have the following pair requests:</Text>
+            <View>
+                <Text style={styles.warnPartAccept}>Accepting an invite shares your data with them</Text>
+                {/* <Text style={styles.message}>You have the following pair requests:</Text> */}
                 {pairRequests.map((request, index) => (
-                    <View key={index}>
-                        <Text style={styles.item}>{request.email} sent you a request</Text>
-                        <TouchableOpacity style={styles.button} onPress={() => handleListAccepting(index)}>
-                            <Text style={styles.buttonText}>{request.isDesired ? "unselect" : "select"}</Text>
-                        </TouchableOpacity>
-                    </View>
+                    <Pressable
+                        key={index}
+                        style={styles.radioItem}
+                        onPress={() => setAcceptPartner(request.email)}
+                    >
+                        <View style={styles.radioCircle}>
+                            {acceptPartner === request.email && <View style={styles.selectedDot} />}
+                        </View>
+                        <Text style={styles.item}>{request.email} </Text>
+                    </Pressable>
                 ))}
                 <TouchableOpacity style={styles.button} onPress={acceptPairRequest}>
-                    <Text style={styles.buttonText}>{"Accept Pair Request from " + acceptPartner}</Text>
+                    <Text style={styles.buttonText} >
+                        Accept invite from{"\n"}
+                        <Text style={{ fontWeight: 'normal' }}>{acceptPartner}</Text>
+                    </Text>
                 </TouchableOpacity>
+                <View style={styles.divider} />
             </View>
         );
     }
 
-    return numPairRequests==0 ? noRequestsReturn() : numPairRequests==1 ? oneRecievedRequestReturn() : multiRecievedRequestsReturn();
+    return numPairRequests == 0 ? noRequestsReturn() : numPairRequests == 1 ? oneRecievedRequestReturn() : multiRecievedRequestsReturn();
 }
 
 const styles = StyleSheet.create({
     container: {
-      width: "83%",
-      paddingVertical: 20,
-      paddingHorizontal: 15,
-      backgroundColor: "white",
-      borderRadius: 16,
-      shadowColor: "#000",
-      shadowOffset: { width: 0, height: 4 },
-      shadowOpacity: 0.2,
-      shadowRadius: 2,
+        width: "83%",
+        paddingVertical: 20,
+        paddingHorizontal: 15,
+        backgroundColor: "white",
+        borderRadius: 16,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.2,
+        shadowRadius: 1,
     },
     button: {
         padding: 8,
-        backgroundColor: '#FF9FB8',
+        backgroundColor: '#EEEEEE',
         alignItems: 'center',
-        borderRadius: 15,
+        borderRadius: 8,
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+        shadowRadius: 1,
+        width: '94%',
+        alignSelf: 'center',
     },
     buttonText: {
-        color: '#fff',
-        fontSize: 16,
+        color: '#4a4a4a',
+        fontSize: 15,
+        lineHeight: 15,
+        textAlign: 'center',
         fontWeight: 'bold',
-        lineHeight: 22,
+    },
+    buttonOneRequest: {
+        width: '94%',
+        padding: 8,
+        backgroundColor: '#EEEEEE',
+        alignItems: 'center',
+        borderRadius: 8,
+        alignSelf: 'center',
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 3 },
+        shadowOpacity: 0.2,
+        shadowRadius: 1,
     },
     message: {
+        padding: 15,
     },
+
+    messageOne: {
+        padding: 15,
+        fontWeight: 'bold',
+    },
+    radioItem: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginBottom: 10,
+        margin: 10,
+    },
+
+    radioCircle: {
+        backgroundColor: '#FFF',
+        height: 22,
+        width: 22,
+        borderRadius: 12.5,
+        borderWidth: 2,
+        borderColor: '#4A4A4A',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginRight: 10,
+    },
+
+    selectedDot: {
+        height: 12,
+        width: 12,
+        borderRadius: 6,
+        backgroundColor: '#4A4A4A',
+    },
+    divider: {
+        width: "100%",
+        height: 1,
+        backgroundColor: "#ccc",
+        marginVertical: 10,
+        marginTop: 20,
+    },
+    warnPartAccept: {
+        marginLeft: 5,
+        color: '#4a4a4a',
+        shadowColor: "#000",
+        shadowOffset: { width: 0, height: 4 },
+        shadowOpacity: 0.2,
+        shadowRadius: 2,
+        padding: 10,
+    }
+
 });
 
 export default SeeRequestsComp;
